b9acd13e872bb782d8bf7ff6dd51ee5e1b4bf6b2
dvidEast <daviddlim11@gmail.com>
2025-06-04 21:56:34 -0700

    changed art link to vsco

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -5,20 +5,25 @@ export default function Navbar() {
     return (
         <nav className={'mt-10 pl-10 pr-10 flex flex-row justify-between items-center'}>
             <div className="flex gap-4 items-center">
+
                 <Link href='/'>DAVID LIM</Link>
+
                 <a href="https://github.com/dvidEast" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                     <FaGithub className="hover:text-white transition-colors duration-200" />
                 </a>
+
                 <a href="https://www.linkedin.com/in/daviddlim" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                     <FaLinkedin className="hover:text-white transition-colors duration-200" />
                 </a>
+
                 <a href="mailto:daviddlim11@gmail.com" aria-label="Email">
                     <FaEnvelope className="hover:text-white transition-colors duration-200" />
                 </a>
             </div>
 
             <div className={'flex flex-row items-center gap-4'}>
-                <Link href='art'>ART</Link>
+                {/* <Link href='art'>ART</Link> */}
+                <a href="https://vsco.co/dvidlimm/gallery#google_vignette" target="_blank" rel="noopener noreferrer" aria-label="Vsco">ART</a>
                 <a target="_blank" href="https://drive.google.com/drive/folders/1xjqC2a8Qb7t931TGmHaiyF_ox69FhAxV?usp=sharing">RESUME</a>
                 {/* <div></div> */}
             </div>
